4c67c6ce07d7fa7a598942244897cccda7abe76f
Allyn Alda <allyn.alda@gft.com>
2019-10-07 08:43:26 +0200

    Updates

---
+++ precache-manifest.5ba6bf75308e910f62614af13326add6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad54cf104f779a41cf3d4621ce12fe44",
+    "url": "/i18n-Easy/index.html"
+  },
+  {
+    "revision": "e4b142e2713d4edeed75",
+    "url": "/i18n-Easy/static/css/main.4c764036.chunk.css"
+  },
+  {
+    "revision": "dc50a86382e2b0f386a1",
+    "url": "/i18n-Easy/static/js/2.4ec535a6.chunk.js"
+  },
+  {
+    "revision": "e4b142e2713d4edeed75",
+    "url": "/i18n-Easy/static/js/main.af91c5f6.chunk.js"
+  },
+  {
+    "revision": "74785da6d7171dcdf244",
+    "url": "/i18n-Easy/static/js/runtime-main.b2f92cc9.js"
+  }
+]);
